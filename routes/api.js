265214568eeb564e7c1c22646b7c6d2f47f13b25
alonchis <23283560+alonchis@users.noreply.github.com>
2020-09-14 09:56:04 -0500

    add reverse unix timestamp route. code housekeeping

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const expressValidator = require('express-validator')
+let dateRgx = new RegExp(/\d{4}-\d{2}-\d{2}/);
+let timestampRgx = new RegExp(/\d+/);
 
-router.get('/timestamp', function(req, res, next) {
-    // res.render('index', { title: 'Express' });
-    // parse request
+router.get('/timestamp', function(req, res) {
     var time = (new Date()).getTime();
     var utcTimestamp = (new Date(time)).toUTCString();
     var response = {timestamp: time, utc: utcTimestamp};
@@ -13,11 +13,22 @@ router.get('/timestamp', function(req, res, next) {
 });
 
 router.get('/timestamp/:date_string', function(req, res, next) {
-    // res.render('index', { title: 'Express' });
-    // parse request
-    var time = (new Date(req.params.date_string)).getTime();
-    var utcTimestamp = (new Date(time)).toUTCString();
-    var response = {unix: time, utc: utcTimestamp};
+    var time;
+    var dateString = req.params.date_string;
+
+    if (dateRgx.test(dateString)) {
+        time = (new Date(dateString)).getTime();
+    } else if (timestampRgx.test(dateString)) {
+        time = (new Date(Number(dateString))).getTime();
+    }
+
+    if (time == null) {
+        response = {error: "invalid date"};
+        res.json(response);
+        return;
+    }
+
+    var response = {unix: time, utc: (new Date(time)).toUTCString()};
     console.log('received request for ' + time + ' is timestamp ' + response.timestamp);
     res.json(response);
 });
