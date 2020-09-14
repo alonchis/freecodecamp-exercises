775d57c50b1d9e20f5ae715cc6722c2373642b82
alonchis <23283560+alonchis@users.noreply.github.com>
2020-09-14 07:18:49 -0500

    initial push to github

---
+++ routes/api.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const expressValidator = require('express-validator')
+
+router.get('/timestamp', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    // parse request
+    var time = (new Date()).getTime();
+    var utcTimestamp = (new Date(time)).toUTCString();
+    var response = {timestamp: time, utc: utcTimestamp};
+    console.log('received request for current timestamp: ' + response);
+    res.json(response);
+});
+
+router.get('/timestamp/:date_string', function(req, res, next) {
+    // res.render('index', { title: 'Express' });
+    // parse request
+    var time = (new Date(req.params.date_string)).getTime();
+    var utcTimestamp = (new Date(time)).toUTCString();
+    var response = {timestamp: time, utc: utcTimestamp};
+    console.log('received request for ' + time + ' is timestamp ' + response.timestamp);
+    res.json(response);
+});
+
+module.exports = router;
